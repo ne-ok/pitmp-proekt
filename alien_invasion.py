ecfdf7c8edf0495d9ef6578141b7ee82091a5891
ne-ok <kruassankina@yandex.ru>
2025-05-18 01:11:22 +0300

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,25 +1,26 @@
 import pygame
+from pygame.sprite import Group
 from settings import Settings  
 from ship import Ship
 import game_functions as gf
 
 def run_game():
     """Инициализирует игру и создает объект экрана."""
-
     pygame.init()
     ai_settings = Settings()
-
-    screen = pygame.display.set_mode(
-        (ai_settings.screen_width, ai_settings.screen_height))
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    # Создание корабля
-    ship = Ship(screen)
+     # Создание корабля.
+    ship = Ship(ai_settings, screen)
+    # Создание группы для хранения пуль.
+    bullets = Group()
 
     # Запуск основного цикла игры.
     while True:
-        gf.check_events(ship)  # Обрабатываем события
-        ship.update()        # Обновляем позицию корабля
-        gf.update_screen(ai_settings, screen, ship) # Обновляем экран
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
