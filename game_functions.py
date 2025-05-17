714399e9fbdf1b4056835a1661a4fc89a737b533
ne-ok <kruassankina@yandex.ru>
2025-05-18 01:12:25 +0300

    Update game_functions.py

--- game_functions.py
+++ game_functions.py
@@ -1,31 +1,54 @@
 import sys
 import pygame
-from time import sleep
+from bullet import Bullet
 
-
-def check_events(ship):
+def check_events(ai_settings, screen, ship, bullets):
     """Обрабатывает нажатия клавиш и события мыши."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(event, ship)
 
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
+    """Реагирует на нажатие клавиш."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        fire_bullet(ai_settings, screen, ship, bullets)
 
-def update_screen(ai_settings, screen, ship):
+def check_keyup_events(event, ship):
+    """Реагирует на отпускание клавиш."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+def update_screen(ai_settings, screen, ship, bullets):
     """Обновляет изображения на экране и отображает новый экран."""
-    # При каждом проходе цикла перерисовывается экран.
-    screen.fill(ai_settings.bg_color)  # Заполняем экран белым цветом
-
-    ship.blitme() # Рисуем корабль
+    screen.fill(ai_settings.bg_color)
+    
+    # Вывод корабля и пуль
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+    ship.blitme()
 
     # Отображение последнего прорисованного экрана.
     pygame.display.flip()
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    """Выпускает пулю, если максимум еще не достигнут."""
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
+
+def update_bullets(bullets):
+    """Обновляет позиции пуль и уничтожает старые пули."""
+    bullets.update()
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
