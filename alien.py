3be67d7e7ebf23f37071a1b9a97fa5433812b50b
ne-ok <kruassankina@yandex.ru>
2025-05-26 22:47:48 +0300

    Update alien.py

--- alien.py
+++ alien.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.sprite import Sprite
 
+
 class Alien(Sprite):
     """Класс, представляющий одного пришельца."""
     def __init__(self, ai_settings, screen):
@@ -8,24 +9,32 @@ class Alien(Sprite):
         super(Alien, self).__init__()
         self.screen = screen
         self.ai_settings = ai_settings
-        self.image = pygame.image.load('C:/Users/kruas/OneDrive/Рабочий стол/LALA/alien_invasion/images/alien.bmp')
-        self.image = pygame.transform.scale(self.image, (50, 50))  # Изменение размера изображения
+
+        # Загрузка изображения пришельца и назначение атрибута rect.
+        self.image = pygame.image.load('C:/Users/kruas/OneDrive/Рабочий стол/LALA/alien_invasion/images/alien.png')
+        self.image = pygame.transform.scale(self.image, (60, 40)) 
         self.rect = self.image.get_rect()
-        self.x = float(self.rect.x)
+
+        # Каждый новый пришелец появляется в левом верхнем углу экрана.
         self.rect.x = self.rect.width
         self.rect.y = self.rect.height
-        self.y = float(self.rect.y)
-
-    def blitme(self):
-        """Выводит пришельца в текущем положении."""
-        self.screen.blit(self.image, self.rect)
+        
+        # Сохранение точной позиции пришельца.
+        self.x = float(self.rect.x)
 
     def check_edges(self):
         """Возвращает True, если пришелец находится у края экрана."""
         screen_rect = self.screen.get_rect()
-        return self.rect.right >= screen_rect.right or self.rect.left <= 0
-
+        if self.rect.right >= screen_rect.right:
+            return True
+        elif self.rect.left <= 0:
+            return True
+        
     def update(self):
-        """Перемещает пришельца влево или вправо в зависимости от направления флота."""
+        """Перемещает пришельца вправо."""
         self.x += (self.ai_settings.alien_speed_factor * self.ai_settings.fleet_direction)
         self.rect.x = self.x
+
+    def blitme(self):
+        """Выводит пришельца в текущем положении."""
+        self.screen.blit(self.image, self.rect)
