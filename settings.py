f9d602181a7fa9e66c7fbf6aa261d7ef28c09082
ne-ok <kruassankina@yandex.ru>
2025-05-26 22:49:41 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,28 +1,47 @@
-class Settings:
-    """Класс для хранения всех настроек игры Alien Invasion."""
-
+class Settings():
     def __init__(self):
-        """Инициализирует настройки игры."""
-        # Параметры экрана
-        self.screen_width = 1400
-        self.screen_height = 775
-        self.bg_color = (230, 230, 230)  # Светло-серый фон
+        """Инициализирует статические настройки игры."""
+
+        # Настройки экрана
+        self.screen_width = 1300
+        self.screen_height = 750
+        self.bg_color = (230, 230, 230)
 
         # Настройки корабля
-        self.ship_speed_factor = 1  # Скорость корабля
+        self.ship_limit = 3
 
-        # Параметры пули
-        self.bullet_speed_factor = 1
+        # Настройки пуль
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = (60, 60, 60)
+        self.bullet_color = 60, 60, 60
         self.bullets_allowed = 3
 
-        # Параметры пришельцев
-        self.alien_speed_factor = 1  # Скорость пришельцев
-        self.fleet_drop_speed = 10  # Насколько опускается флот при смене направления
-        self.fleet_direction = 1    # 1 - движение вправо, -1 - влево
+        # Настройки пришельцев
+        self.fleet_drop_speed = 10
 
-        self.alien_speed_factor = 0.5  # уменьшили скорость пришельцев
-        self.fleet_direction = 1  # направление движения флота (1 — вправо, -1 — влево)
-        self.fleet_drop_speed = 10  # скорость опускания флота вниз
+        # Темп ускорения игры
+        self.speedup_scale = 1.1
+
+        # Темп роста стоимости пришельцев
+        self.score_scale = 1.5
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Инициализирует настройки, изменяющиеся в ходе игры."""
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3
+        self.alien_speed_factor = 1
+
+        # fleet_direction = 1 обозначает движение вправо; а -1 - влево
+        self.fleet_direction = 1
+
+        # Подсчет очков
+        self.alien_points = 50
+
+    def increase_speed(self):
+        """Увеличивает настройки скорости и стоимость пришельцев."""
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
+        self.alien_points = int(self.alien_points * self.score_scale)
+        print(self.alien_points)
